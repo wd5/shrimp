5e7c5c82a0ecfe070e8eba1a641d36e423e9a74f
Paul O'Leary McCann <polm@dampfkraft.com>
2011-12-19 01:06:17 +0900

    Mostly functional! Added lots of resources. -POLM

--- shrimp.js
+++ shrimp.js
@@ -1,5 +1,16 @@
 //Major globals
-var levels = {1: {fill: 0.6, caption: "Once there was a boy."}}
+var levels = {1: {fill: 0.4, caption: "Once there was a boy."},
+		2: {fill: 0.4, caption: "His parents were scientists."},
+		3: {fill: 0.5, caption: "They worked on an important project."},
+		4: {fill: 0.5, caption: "A meteor fell from space."},
+		5: {fill: 0.5, caption: "The meteor contained a powerful energy."},
+		6: {fill: 0.6, caption: "It was taken to a lab."},
+		7: {fill: 0.6, caption: "Research progressed."},
+		8: {fill: 0.7, caption: "The boy went to visit his parents."},
+		9: {fill: 0.7, caption: "They were called away."},
+		10: {fill: 0.8, caption: "He was alone."},
+		11: {fill: 0.9, caption: "He wandered around."},
+		12: {fill: 0.2, caption: "He thought it would be nice to have a friend."}}
 
 
 //Helper functions
@@ -18,6 +29,7 @@ function mazegen(y,x){
 	if (start > x) {startcell.y = start - x;}
 	else {startcell.x = start;}
 
+	//TODO fix bug here...
 	maze[c(startcell.x)][c(startcell.y)] = 1;
 	var cc = startcell;
 	var opcells = new Array();
@@ -61,9 +73,9 @@ function mazegen(y,x){
 				maze[i][j] = 0;
 			}
 		}
-		console.log(line);
+		//console.log(line);
 	}
-	console.log(maze);
+	//console.log(maze);
 	return maze;
 }
 
@@ -77,6 +89,7 @@ function decay(map, fill){
 	return map;
 }
 
+
 function maptrans(map){
 	//Given a map with descriptive tile names, changes them to numeric values
 	var i,j;
@@ -89,22 +102,42 @@ function maptrans(map){
 	return map;
 }
 
+function border(map){
+	//Given a map, make a solid border around it
+	//Assume 0 is open and 1 is solid
+	var nx = map.length + 2;
+	var ny = map[0].length + 2;
+	var nmap = new Array(nx);
+	var i,j;
+	for(i=0;i<nx;i++){
+		nmap[i] = new Array(ny);
+		for(j=0;j<ny;j++){
+			if(i === 0 || i === nx-1 || j === 0 || j === ny-1){nmap[i][j]=1;}
+			else {nmap[i][j] = map[i-1][j-1];}
+		}
+	}
+	return nmap;
+}
+
 window.onload = function () {
 	enchant();
  
-	var game = new Game(640, 480);
+	var game = new Game(320, 240);
 	game.fps = 30;
-	game.scale = 1;
+	game.scale = 2;
 	//Resources
 	game.preload("player.gif");
 	game.preload("map.png");
 	game.preload("darkness.png");
+	game.preload("match.png");
+	game.preload("goal.png");
+	//Sounds
+	game.preload("death.wav", "matchcount.wav", "matchlight.wav", "matchpickup.wav", "shrimp.wav", "warp.wav");
 	//Keybindings
 	game.keybind(90,"a");
+	game.keybind(88,"b");
 	var level = 1;
 	var paused = false;
-
-	
 
 	game.onload = function () {
 		var player = new Sprite(16,16);
@@ -127,7 +160,7 @@ window.onload = function () {
 		player.matches = 5;
 
 		player.addEventListener('enterframe', function(e){
-			if(paused){return;}
+			if(paused){console.log("paused");return;}
 			//Input checks
 			var x = player.x;
 			var y = player.y;
@@ -139,8 +172,9 @@ window.onload = function () {
 
 			counter++;
 			if(counter===10){counter=0; margin--;}
-			if(margin===0){
+			if(margin===8){
 				//TODO play a creepy noise
+				game.assets["death.wav"].play();
 				//Just restart the level
 				startlevel(level);
 			}
@@ -164,43 +198,61 @@ window.onload = function () {
 		});
 
 		game.addEventListener(Event.A_BUTTON_DOWN, function(e){
+			console.log("a button");
 			//Light a match
 			if(player.matches === 0){
 				//No matches - nice knowing you!
-				//TODO play sad noise
+				game.assets["death.wav"].play();
 			} else {
 				//Reset darkness
-				//TODO play match noise
+				game.assets["matchlight.wav"].play();
 				margin = 16 * 5;
 				player.matches--;
 			}
 		});
 
+		function matchcount(c){ return (c === 0) ? null : function (){game.assets["matchcount.wav"].play(); setTimeout(matchcount(c-1), 200);};}
+
 		game.addEventListener(Event.B_BUTTON_DOWN, function(e){
+			console.log("b button");
 			//Count your matches
 			if(player.matches === 0){
-				//TODO play sad noise
+				game.assets["death.wav"].play();
 			}else{
 				var i;
-				for(i=0;i<player.matches;i++){
-					//TODO play click
-				}
+				setTimeout(matchcount(player.matches), 200);
 			}
 		});
 
 		var startlevel = function(level){
+			//TODO level 13 = game over you won
 			var fill = levels[level].fill;
 			var caption = levels[level].caption;
-			//TODO show caption
-			//TODO create stage
-			var map = new Map(16,16);
-			var maze = decay(maptrans(mazegen(20,16)),0.7);
-			map.loadData(maze);
-			map.collisionData = maze;
+			//show caption
+			//create stage
+			map = new Map(16,16);
+			map.image = game.assets["map.png"];
+			var mx = 20;
+			var my = 16;
 			var scene = new Scene();
-
 			scene.addChild(map);
+			var maze = addgoal(border(decay(maptrans(mazegen(20,16)),0.7)),scene);
+			map.collisionData = maze;
+			map.loadData(maze);
+			
+			//place player -  random player start location
+			//Always safe because of how the maze algo works
+			player.x = (2 * 16 * Math.floor(Math.random() * mx)) + 16;
+			player.y = (2 * 16 * Math.floor(Math.random() * my)) + 16;
+			//place goal - put it far away from the player
+			//TODO
+			//place matches - common, but not too common
+			//TODO
+
+
+
 			scene.addChild(player);
+			addmatches(maze,0.03, scene);
 			scene.backgroundColor = "#f0f";
 			for(i=0;i<darkness.length;i++){scene.addChild(darkness[i]);}
 
@@ -208,19 +260,23 @@ window.onload = function () {
 			var label = new Label(caption);
 			leveltext.backgroundColor = "#000";
 			label.color = "#fff";
+			label.y = (game.height / 2);
+			label.x = (game.width / 2) - (label.width / 2);
 			leveltext.addChild(label);
 			leveltext.countdown = 60;
 			leveltext.addEventListener('enterframe',function(e){
 				//This should only display for a few seconds
-				if(this.countdown===0) {paused = false; game.popScene();}
+				if(this.countdown===0) {paused = false; game.popScene();return;}
 				paused = true;
 				this.countdown--;
 			});
 
 			scene.addEventListener("enterframe", function(e){
-				stage.x = (game.width / 2) - player.x;
-				stage.y = (game.height / 2) - player.y;
+				this.x = (game.width / 2) - player.x;
+				this.y = (game.height / 2) - player.y;
 			});
+
+			margin = 16 * 6;
 
 			game.popScene();
 			game.pushScene(scene);
@@ -228,6 +284,85 @@ window.onload = function () {
 
 		}
 
+		var Match = enchant.Class.create(enchant.Sprite, {initialize: function(x,y){
+			enchant.Sprite.call(this,16,16);
+			this.image = game.assets["match.png"];
+			this.x = x * 16;
+			this.y = y * 16;
+			this.addEventListener('enterframe', function(e){
+				if(this.opacity != 0 && this.intersect(player)){
+					console.log("intersected!");
+					//TODO play noise
+					player.matches++;
+					this.opacity = 0;
+					game.rootScene.removeChild(this);
+					delete this;
+				}
+			});
+
+		}});
+
+		function addmatches(map, fill, scene){
+			var i,j;
+			for(i=0;i<map.length;i++){
+				for(j=0;j<map[0].length;j++){
+					if(map[i][j] === 0 && Math.random() < fill){
+						//put in a match if lucky
+						var match = new Match(j,i);
+						scene.addChild(match);
+					}
+				}
+			}
+			return;
+		}
+
+		var Goal = enchant.Class.create(enchant.Sprite, {initialize: function(x,y){
+			enchant.Sprite.call(this,16,16);
+			this.image = game.assets["goal.png"];
+			this.x = x * 16;
+			this.y = y * 16; 
+			this.basicy = this.y;
+			this.bounce = 0;
+			this.bouncedir = 1;
+			this.addEventListener('enterframe', function(e){
+				this.bounce += this.bouncedir;
+				if(this.bounce < 0){this.bouncedir = 1; this.bounce = 0;}
+				if(this.bounce > 5){this.bouncedir = -1; this.bounce = 5;}
+
+				this.y = this.basicy + this.bounce; 
+
+				if(this.intersect(player)){
+					game.assets["warp.wav"].play();
+					console.log("found goal!");
+					level++;
+					startlevel(level);
+					game.rootScene.removeChild(this);
+					delete this;
+				}
+			});
+
+		}});
+
+		function addgoal(map, scene){
+			//Place the level end
+			//It needs to be far from the player
+			var width = map.length;
+			var height = map[0].length;
+			var x = (player.x / 16 < map.width / 2) ? map.width - Math.floor(Math.random() * 10) + 4  : Math.floor(Math.random() * 10) + 4;
+			var y = (player.y / 16 < map.height / 2) ? map.height - Math.floor(Math.random() * 10) + 4 : Math.floor(Math.random() * 10) + 4;
+			var i,j;
+			for(i=x-2;i<x+3;i++){
+				for(j=y-2;j<y+3;j++){
+					map[j][i] = 0;
+				}
+			}
+			//Add the goal point
+			console.log("goal: " + x + ":" + y);
+			var goal = new Goal(x,y);
+			scene.addChild(goal);
+			return map;
+		}
+	/*
 		var map = new Map(16,16);
 		map.image = game.assets["map.png"];
 		var maze = decay(maptrans(mazegen(20,16)),0.7);
@@ -238,19 +373,12 @@ window.onload = function () {
 		stage.addChild(map);
 		stage.addChild(player);
 		for(i=0;i<darkness.length;i++){stage.addChild(darkness[i]);}
-
-		game.rootScene.addEventListener("enterframe", function(e){
-			stage.x = (game.width / 2) - player.x;
-			stage.y = (game.height / 2) - player.y;
-		});
+		*/
 
 		//game.rootScene.addChild(stage);
 		startlevel(1);
 		//game.rootScene.backgroundColor = "#ff000f";
 	};
-
-
-
 
 	//TODO logic 
 	game.start();
